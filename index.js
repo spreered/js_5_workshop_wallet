a14593b07d4949595ba7e8c8750598302273585d
Parker <ygfert@gmail.com>
2020-08-19 12:25:01 +0800

    local storage get and set

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 window.addEventListener("DOMContentLoaded", function() {
   const inputForm = document.forms[1];
+  let local = [];
   inputForm.addEventListener("submit", function(e) {
-    // e.preventDefault();
+    e.preventDefault();
     let revenue = {
       date: inputForm.date.value,
       category: inputForm.category.value,
       amount: inputForm.amount.value,
       description: inputForm.description.value
     }
-    console.log(revenue);
+    local.push(revenue);
+    localStorage.setItem("records", JSON.stringify(local));
+    console.log(JSON.parse(localStorage.getItem("records")));
   })
 });
