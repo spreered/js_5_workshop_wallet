8448f7a71458b0bd8348245baca2bed8c50aeaf7
will Liu <wye139629@gmail.com>
2020-08-19 13:36:22 +0800

    finish task 2

--- index.js
+++ index.js
@@ -7,8 +7,7 @@ form.addEventListener('submit',function(e){
   let date = form.elements['date'].value
   let amount = form.elements['amount'].value
   let description = form.elements['description'].value
-  let arrayObj = []
-  let newArray =[]
+  let arrayObj = [] 
   let getItem = localStorage.getItem('records')
   let listObj = {
     date,
@@ -17,9 +16,24 @@ form.addEventListener('submit',function(e){
     description
   }
   arrayObj.push(listObj)
-  console.log(arrayObj)
-
+  // console.log(listObj)
+  // localStorage.removeItem('records')
+  if (!getItem){
+    localStorage.setItem('records',JSON.stringify(arrayObj))
+  }else{
+    let exists = localStorage.getItem('records')
+    let existsArray = JSON.parse(exists)
+    console.log(existsArray)
+    console.log(listObj)
+    existsArray.push(listObj)
+    console.log(existsArray)
+    localStorage.setItem('records',JSON.stringify(existsArray))
+  }
   
+  // else{
+  //   newArray =  JSON.parse(getItem).push(listObj)
+  //   localStorage.setItem('records',JSON.stringify(newArray))
+  // }
 
 })
 })
