cc38aa3b351315fcd3671b30cbe138f33d951362
Jerry <Jerry19920702@gmail.com>
2020-08-19 15:35:45 +0800

    task2 finished

--- function.js
+++ function.js
@@ -1,14 +1,21 @@
-window.addEventListener('DOMContentLoaded', function(){
+window.addEventListener('DOMContentLoaded', function () {
   var formSubmit = document.forms[1]
   formSubmit.addEventListener('submit', (e) => {
     e.preventDefault();
-    console.log({
+ 
+    var oldItems = JSON.parse(localStorage.getItem('records')) || [];
+    var newItem = {
       category: formSubmit['category'].value,
       date: formSubmit['date'].value,
       amount: formSubmit['amount'].value,
       description: formSubmit['description'].value,
-    })
+    };
+
+    oldItems.push(newItem)
     
+    localStorage.setItem('records', JSON.stringify(oldItems));
+    
+
     formSubmit['category'].value = '';
     formSubmit['date'].value = '';
     formSubmit['amount'].value = '';
