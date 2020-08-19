5f07d6f89051fedce686fc25c0e13b7246ad7639
Andrew <jrnalts@gmail.com>
2020-08-19 12:28:31 +0800

    task_1 done

---
+++ index.js
@@ -0,0 +1,30 @@
+window.addEventListener("DOMContentLoaded", function () {
+	const form = document.forms["item-form"];
+	form.addEventListener("submit", function (e) {
+		e.preventDefault();
+		console.log(
+			JSON.stringify({
+				uuid: `${generateUUID()}`,
+				category: form["category"].value,
+				date: form["date"].value,
+				amount: form["amount"].value,
+				description: form["description"].value,
+			})
+		);
+		form["description"].value = "";
+		form["amount"].value = "";
+	});
+
+	function generateUUID() {
+		var d = new Date().getTime();
+		var uuid = "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(
+			/[xy]/g,
+			function (c) {
+				var r = (d + Math.random() * 16) % 16 | 0;
+				d = Math.floor(d / 16);
+				return (c == "x" ? r : (r & 0x3) | 0x8).toString(16);
+			}
+		);
+		return uuid;
+	}
+});
