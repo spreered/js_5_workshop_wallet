b27b58ca53331d11049709fee44ff15833a71740
莊哲瑋 <f841u04@gmail.com>
2020-08-26 10:31:38 +0800

    task1

---
+++ index.js
@@ -0,0 +1,39 @@
+const itemForm = document.forms["item-form"]
+
+document.addEventListener("DOMContentLoaded", function(){
+  renderRecords();
+});
+
+itemForm.addEventListener("submit", function(e){
+  e.preventDefault();
+  let result = {
+    uuid: generateUUID(),
+    date: itemForm.elements["date"].value,
+    category: itemForm.elements["category"].value,
+    description: itemForm.elements["description"].value,
+    amount: itemForm.elements["amount"].value
+  }
+})
+
+
+
+
+
+
+
+
+
+
+
+
+// Utility
+
+function generateUUID() {
+  var d = new Date().getTime();
+  var uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+    var r = (d+ Math.random()*16)%16 | 0;
+    d = Math.floor(d/16);
+    return (c=='x' ? r : (r&0x3|0x8)).toString(16);
+  });
+  return uuid;
+};
